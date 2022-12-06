3a164eb737bccc537c3332b49fb4eb13def9d064
Evgeny Pavlovsky <evgeny@curlybr.com>
2022-12-06 05:02:49 +0300

    Lesson7 tasks done

---
+++ Lesson7/Restaurant.Messages/InMemoryDb/DatabaseTransaction.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace Restaurant.Messages.InMemoryDb
+{
+	public class DatabaseTransaction
+	{
+		private readonly Guid guid;
+		private readonly ILogger _logger;
+
+		public DatabaseTransaction(ILogger logger)
+		{
+			_logger = logger;
+			guid = Guid.NewGuid();
+			_logger.LogTrace("Транзакция {Transaction} начата.", guid);
+		}
+
+		public void Commit()
+		{
+			_logger.LogTrace("Транзакция {Transaction} успешно завершена.", guid);
+		}
+
+		public void Rollback()
+		{
+			_logger.LogWarning("Транзакция {Transaction} отменена.", guid);
+		}
+	}
+}
---
+++ Lesson7/Restaurant.Messages/InMemoryDb/ProcessedMessageRepository.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Extensions.Logging;
+using System.Collections.Concurrent;
+
+namespace Restaurant.Messages.InMemoryDb
+{
+	public class ProcessedMessageRepository
+	{
+		private readonly ConcurrentDictionary<string, DateTime> repo = new ();
+		private readonly ILogger _logger;
+
+		public ProcessedMessageRepository(ILogger logger)
+		{
+			_logger = logger;
+			Cleanup();  // Запускаем службу очистки
+		}
+
+		public bool TryAddMessage(string MessageId)
+		{
+			return repo.TryAdd(MessageId, DateTime.Now);
+		}
+
+		public async void Cleanup()
+		{
+			while (true)
+			{
+				await Task.Delay(1000);
+				// Каждую секунду просматриваем хранилище
+				foreach (var message in repo)
+					if ((DateTime.Now - message.Value).TotalSeconds > 30)  // Если с создания объекта до текущего момента прошло больше 30 секунд
+						if (repo.TryRemove(message))  // - пытаемся удалить и сообщаем
+							_logger.LogDebug("Сообщение {MessageId} устарело и удалено из хранилища", message.Key);
+			}
+		}
+	}
+}
